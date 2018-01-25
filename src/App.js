c20189816e1295175fe8af16cae93be2097d075a
James Smith <noletubby77@hotmail.com>
2018-01-25 14:11:05 -0500

    updated App.js with routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import Profile from './components/Profile'; ;
+import Film from './components/Film'; 
+import Footer from '/components/Footer'; 
+import Portfolio from '/components/Portfolio'; 
+import Characters from '/components/Characters';
+import {Route, Switch, Redirect} from 'react-router-dom';   
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
+        <Profile />
+          
+          <main>
+      <Switch>
+        <Route path='/:id' component={Characters} />
+        <Route exact path="/" component={Portfolio} />
+      </Switch>
+        </main>
+          
+
+        
       </div>
     );
   }
