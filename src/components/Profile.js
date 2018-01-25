39dc1eb5c095e5dbd90e58bbd318d2d27963bd6a
James Smith <noletubby77@hotmail.com>
2018-01-25 12:35:22 -0500

    logo and quote added to profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,1 +1,18 @@
-Profile.js
+import React from 'react';
+
+export default class Profile extends React.Component {
+	
+
+	render() {
+		const logo = <img src="http://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo.png"/>
+		return (
+			<div>
+				<img src="http://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo.png"/>
+				<h3> A long time ago in a galaxy far, <br /> far away . . .</h3> 
+		
+
+
+			</div>
+		);
+	}
+}
