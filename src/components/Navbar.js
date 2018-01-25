e180345ee6a157c98f72e85392bb64a1ec611fb1
James Smith <noletubby77@hotmail.com>
2018-01-25 12:02:13 -0500

    nav bar updated to include links to page sections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,15 @@
-Navbar.js
+import React from 'react';
+
+export default class Navbar extends React.Component {
+	
+	
+	render() {
+		return (
+			<nav className="nav-bar">
+					<a href="#profile">Star Wars</a>
+					<a href="#films">Films</a>
+					<a href="#contact">Contact</a>
+			</nav>
+		);
+	}
+}
